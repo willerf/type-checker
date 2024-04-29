8d0265e91380409e3cf463db7df1db357e4fc4e4
willerf <erfwill@gmail.com>
2024-04-28 22:13:35 -0700

    default visits

--- src/transformations/visitor.cc
+++ src/transformations/visitor.cc
@@ -1,13 +1,63 @@
 
 #include "visitor.h"
 
+#include <memory>
 #include <vector>
 
-#include "stmt_block_node.h"
+#include "assign_node.h"
+#include "binary_expr_node.h"
+#include "call_node.h"
 #include "fn_node.h"
+#include "if_node.h"
+#include "literal_node.h"
+#include "ret_node.h"
+#include "stmt_block_node.h"
+#include "unary_expr_node.h"
+#include "var_access_node.h"
 
 template<>
 void Visitor<void>::visit(std::shared_ptr<ASTNode> node) {}
+
+template<>
+void Visitor<void>::visit(std::shared_ptr<AssignNode> node) {
+    node->rhs->accept(*this);
+}
+
+template<>
+void Visitor<void>::visit(std::shared_ptr<BinaryExprNode> node) {
+    node->lhs->accept(*this);
+    node->rhs->accept(*this);
+}
+
+template<>
+void Visitor<void>::visit(std::shared_ptr<CallNode> node) {
+    for (auto arg : node->args) {
+        arg->accept(*this);
+    }
+}
+
+template<>
+void Visitor<void>::visit(std::shared_ptr<FnNode> node) {
+    node->stmts->accept(*this);
+}
+
+template<>
+void Visitor<void>::visit(std::shared_ptr<IfNode> node) {
+    node->condition->accept(*this);
+    node->thens->accept(*this);
+    if (node->elses) {
+        node->elses->accept(*this);
+    }
+}
+
+template<>
+void Visitor<void>::visit(std::shared_ptr<LiteralNode> node) {
+}
+
+template<>
+void Visitor<void>::visit(std::shared_ptr<RetNode> node) {
+    node->expr->accept(*this);
+}
 
 template<>
 void Visitor<void>::visit(std::shared_ptr<StmtBlockNode> node) {
@@ -17,15 +67,75 @@ void Visitor<void>::visit(std::shared_ptr<StmtBlockNode> node) {
 }
 
 template<>
-void Visitor<void>::visit(std::shared_ptr<FnNode> node) {
-    node->stmts->accept(*this);
+void Visitor<void>::visit(std::shared_ptr<UnaryExprNode> node) {
+    node->expr->accept(*this);
 }
 
+template<>
+void Visitor<void>::visit(std::shared_ptr<VarAccessNode> node) {
+}
 
 template<>
 std::shared_ptr<ASTNode>
 Visitor<std::shared_ptr<ASTNode>>::visit(std::shared_ptr<ASTNode> node) {
     return node;
+}
+
+template<>
+std::shared_ptr<ASTNode>
+Visitor<std::shared_ptr<ASTNode>>::visit(std::shared_ptr<AssignNode> node) {
+    auto rhs = node->rhs->accept(*this);
+    return make_assign(node->lhs, rhs);
+}
+
+template<>
+std::shared_ptr<ASTNode>
+Visitor<std::shared_ptr<ASTNode>>::visit(std::shared_ptr<BinaryExprNode> node) {
+    auto lhs = node->lhs->accept(*this);
+    auto rhs = node->rhs->accept(*this);
+    return make_binary_expr(lhs, node->op, rhs);
+}
+
+template<>
+std::shared_ptr<ASTNode>
+Visitor<std::shared_ptr<ASTNode>>::visit(std::shared_ptr<CallNode> node) {
+    std::vector<std::shared_ptr<ASTNode>> args;
+    for (auto arg : node->args) {
+        args.push_back(arg->accept(*this));
+    }
+    return make_call(node->proc_name, args);
+}
+
+template<>
+std::shared_ptr<ASTNode>
+Visitor<std::shared_ptr<ASTNode>>::visit(std::shared_ptr<FnNode> node) {
+    auto result = node->stmts->accept(*this); 
+    return make_fn(node->params, result);
+}
+
+template<>
+std::shared_ptr<ASTNode>
+Visitor<std::shared_ptr<ASTNode>>::visit(std::shared_ptr<IfNode> node) {
+    auto condition = node->condition->accept(*this);
+    auto thens = node->thens->accept(*this);
+    std::shared_ptr<ASTNode> elses = nullptr;
+    if (node->elses) {
+        elses = node->elses->accept(*this);
+    }
+    return make_if(condition, thens, elses);
+}
+
+template<>
+std::shared_ptr<ASTNode>
+Visitor<std::shared_ptr<ASTNode>>::visit(std::shared_ptr<LiteralNode> node) {
+    return std::static_pointer_cast<ASTNode>(node);
+}
+
+template<>
+std::shared_ptr<ASTNode>
+Visitor<std::shared_ptr<ASTNode>>::visit(std::shared_ptr<RetNode> node) {
+    auto expr = node->expr->accept(*this);
+    return make_ret(expr);
 }
 
 template<>
@@ -40,7 +150,14 @@ Visitor<std::shared_ptr<ASTNode>>::visit(std::shared_ptr<StmtBlockNode> node) {
 
 template<>
 std::shared_ptr<ASTNode>
-Visitor<std::shared_ptr<ASTNode>>::visit(std::shared_ptr<FnNode> node) {
-    auto result = node->stmts->accept(*this); 
-    return make_fn(node->params, result);
+Visitor<std::shared_ptr<ASTNode>>::visit(std::shared_ptr<UnaryExprNode> node) {
+    auto expr = node->expr->accept(*this);
+    return make_unary_expr(node->op, expr);
 }
+
+template<>
+std::shared_ptr<ASTNode>
+Visitor<std::shared_ptr<ASTNode>>::visit(std::shared_ptr<VarAccessNode> node) {
+    return std::static_pointer_cast<ASTNode>(node);
+}
+
--- src/transformations/visitor.h
+++ src/transformations/visitor.h
@@ -4,15 +4,31 @@
 #include <memory>
 
 struct ASTNode;
-struct StmtBlockNode;
+struct AssignNode;
+struct BinaryExprNode;
+struct CallNode;
 struct FnNode;
+struct IfNode;
+struct LiteralNode;
+struct RetNode;
+struct StmtBlockNode;
+struct UnaryExprNode;
+struct VarAccessNode;
 
 template<typename T>
 class Visitor {
   public:
     virtual T visit(std::shared_ptr<ASTNode>);
-    virtual T visit(std::shared_ptr<StmtBlockNode>);
+    virtual T visit(std::shared_ptr<AssignNode>);
+    virtual T visit(std::shared_ptr<BinaryExprNode>);
+    virtual T visit(std::shared_ptr<CallNode>);
     virtual T visit(std::shared_ptr<FnNode>);
+    virtual T visit(std::shared_ptr<IfNode>);
+    virtual T visit(std::shared_ptr<LiteralNode>);
+    virtual T visit(std::shared_ptr<RetNode>);
+    virtual T visit(std::shared_ptr<StmtBlockNode>);
+    virtual T visit(std::shared_ptr<UnaryExprNode>);
+    virtual T visit(std::shared_ptr<VarAccessNode>);
 
     virtual ~Visitor() {}
 };
